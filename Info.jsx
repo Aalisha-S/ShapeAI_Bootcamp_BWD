6652c88c3a18c32c025ce1bf0d50a68b1e3e9219
Aalisha-S <86483472+Aalisha-S@users.noreply.github.com>
2021-06-26 22:38:52 +0530

    Update Info.jsx

--- Info.jsx
+++ Info.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 function Info(){
-  return(
+  return (
     <div className="note">
       <h1>Javascript and React.js</h1>
-      <p>A basic web dev React js Bootcamp</p>
+      <p>This free bootcamp was organised from 15th June for free and taught us the basics of web development.</p>
     </div>
   );
 }
